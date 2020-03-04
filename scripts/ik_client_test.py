35cc7a1f221e5a3529d68398f4cc5e09865e9bc4
David Wannheden <david@wannheden.com>
2020-03-04 14:03:32 +0100

    client and service now work, sends string returns float64

--- scripts/ik_client_test.py
+++ scripts/ik_client_test.py
@@ -9,11 +9,14 @@ def ik_client(string):
     rospy.wait_for_service('Compute_Inverse_Kinematics')
     print("2")
     try:
+        print(4)
         handle_compute_ik = rospy.ServiceProxy('Compute_Inverse_Kinematics', InverseKinematics)
+        print(5)
         resp1 = handle_compute_ik(string)
-        return resp1.sum
+        print(6)
+        return resp1.solution
     except rospy.ServiceException:
-        print ("Service call failed")
+        print("Service call failed")
 
 def usage():
     #return "%s [x y]"%sys.argv[0]
--- scripts/ik_service_test.py
+++ scripts/ik_service_test.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
-from basic_py_scripts.srv import InverseKinematics
+from basic_py_scripts.srv import InverseKinematics, InverseKinematicsResponse
 import rospy
 
 
-# from numpy.core import float64[]
+#from numpy.core import float64[]
 
 
 def handle_compute_ik(req):
     print("Returning goal state angles")
     #print("you requested " + req)
     print(req)
-    solution = [-1.6031114799568484, -0.3187570069269499, -2.5669119402583984, -1.2030817908523226, 2.403965512561466,
-                0.7742537749351657, -1.3350190150363965]
+    #solution = [-1.6031114799568484, -0.3187570069269499, -2.5669119402583984, -1.2030817908523226, 2.403965512561466,
+    #            0.7742537749351657, -1.3350190150363965]
+    solution = 1
+    print(solution)
     print("3.1")
     return solution
 
