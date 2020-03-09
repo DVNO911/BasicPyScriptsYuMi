310ced4e454ff13d8a9b1630494fb523e7147a23
DVNO911 <david@wannheden.com>
2020-03-09 19:23:35 +0100

    troubleshooting sending poses to service

--- scripts/ik_client_test2.py
+++ scripts/ik_client_test2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Client that can send a pose and recieve a solution
+# Client that can send a pose and receive a solution
 
 import sys
 import rospy
@@ -10,9 +10,14 @@ from basic_py_scripts.srv import InverseKinematics2
 
 def ik_client(string1, string2, posestamp):
     rospy.wait_for_service('Compute_Inverse_Kinematics2')
+    print(2)
     try:
         handle_compute_ik2 = rospy.ServiceProxy('Compute_Inverse_Kinematics2', InverseKinematics2)
-        resp1 = handle_compute_ik2(string1, string2, posestamp)
+        print(3)
+        req = basic_py_scripts.srv.InverseKinematics2Request(string1, string2, posestamp)
+        print(3.1)
+        resp1 = handle_compute_ik2(req)
+        print(4)
         return resp1.solution
     except rospy.ServiceException:
         print("Service call failed")
@@ -22,7 +27,7 @@ def generate_posestamped():
     poseS = PoseStamped()
 
     poseS.header = std_msgs.msg.Header()
-    poseS.header.stamp = rospy.Time.now()  # Note you need to call rospy.init_node() before this will work
+    #poseS.header.stamp = rospy.Time.now()  # Note you need to call rospy.init_node() before this will work
 
     poseS.pose.position.x = 1
     poseS.pose.position.y = 2
@@ -39,13 +44,24 @@ def usage():
 
 if __name__ == "__main__":
 
-    posestamp = generate_posestamped()
-    if len(sys.argv) == 3:
+    #posestamp = generate_posestamped()
+    #print(posestamp)
+    #print("END POSE")
+    print("here")
+    print(sys.argv[1])
+    print("555555555555555555")
+    print(sys.argv[2])
+    print("!!!!!!!!!!!!!!!!!!!")
+    print(sys.argv[3])
+    print("and here")
+    print(len(sys.argv))
+    if len(sys.argv) == 4:
         x = str(sys.argv[1])
         y = str(sys.argv[2])
+        posestamp = sys.argv[3]
     else:
         print(usage())
         sys.exit(1)
     print("Requesting ...")
-    print(100)
+    posestamp = generate_posestamped()
     print("%s => %s" % (x, ik_client(x, y, posestamp)))
