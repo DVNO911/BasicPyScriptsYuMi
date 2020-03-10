50ffe2c4917d7a5f0ae58e79b468b188b42e5fbe
DVNO911 <david@wannheden.com>
2020-03-10 10:06:38 +0100

    client & service can now send custom messages

--- scripts/ik_service_test2.py
+++ scripts/ik_service_test2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from basic_py_scripts.srv import InverseKinematics2, InverseKinematics2Response
+from basic_py_scripts.msg import *
 
 from std_msgs.msg import *
 from geometry_msgs import *
@@ -14,12 +15,14 @@ def handle_compute_ik2(req):
     print(req.tooltip_name)
     print(req.posestamp)
     print("Returning goal state angles")
-    solution = [-1.6031114799568484, -0.3187570069269499, -2.5669119402583984, -1.2030817908523226, 2.403965512561466,
+    angles = [-1.6031114799568484, -0.3187570069269499, -2.5669119402583984, -1.2030817908523226, 2.403965512561466,
                 0.7742537749351657, -1.3350190150363965]
-    #solution = [1, 1]
-    print(solution)
-    return InverseKinematics2Response(2, solution)
-
+    sol = Solution()
+    sol.status = 1
+    sol.ik_solution = angles
+    print(sol)
+    #return InverseKinematics2Response(2, solution)
+    return sol
 
 def ik_server():
     rospy.init_node('ik_server')
