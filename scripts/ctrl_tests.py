0bc539c71dad9a770bf6b57aad49f3fd54bb4775
DVNO911 <david@wannheden.com>
2020-03-11 15:36:55 +0100

    more work in ctrl_tests, still a bit unclear how to model pidcontroller

--- scripts/ctrl_tests.py
+++ scripts/ctrl_tests.py
@@ -1,17 +1,25 @@
 # pip3 install control
+# pip3 install scipy
 # pip3 install matplotlib
 # sudo apt-get install python3-tk
 # this file is for testing control package in python, so we can later build our control system for yumi
-import numpy
+from scipy import signal
+import numpy as np
 import matplotlib.pyplot as plt
 import control as cl
+from simple_pid import PID
 
 num = [0.5]
 den = [1, 0.5]
 
-sys = cl.TransferFunction(num, den) #create Transfer Function
-print(cl.pole(sys)) #print pole
-print(cl.zero(sys)) #print zero
+G = cl.TransferFunction(num, den) # create Transfer Function
+H = cl.TransferFunction(1,1)
+pid = PID(1, 0.2, 0, 1) # setpoint = 1 # not sure if i want to use this
+
+I = cl.feedback(G, H)
+
+print(cl.pole(G)) #print pole
+print(cl.zero(G)) #print zero
 
 
 # create bode plot
@@ -19,9 +27,13 @@ print(cl.zero(sys)) #print zero
 
 
 # plot step response
-T, yout = cl.step_response(sys)
-plt.plot(T,yout)
+T, yout = cl.step_response(I)
+plt.plot(T, yout)
 
-
+# sine wave & square wave for generating reference signals
+time = np.arange(0, 10, 0.1);
+amplitude = np.sin(time)
+# plt.plot(time, signal.square(2*np.pi*time))
+# plt.plot(time, amplitude)
 # show bode plot
 plt.show()
